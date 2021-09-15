59aef62bd430e34f828efdf6942729f4b39710fa
Henri Immonen <henri.immonen000@gmail.com>
2021-09-15 16:56:49 +0300

    Added render_templates import

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 # First page for 'UniMarket' - Still in Development!
 
 @app.route("/")
 def index():
-    return "Work in progress"
+    return render_template("index.html")
