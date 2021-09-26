7778b8dc962b6cdd48e6262b88f3ccfb2ff71c5c
Henri Immonen <henri.immonen000@gmail.com>
2021-09-26 17:51:24 +0300

    schema.sql updated to postgresql-form

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE listings (id SERIAL PRIMARY KEY , header TEXT, location TEXT, content TEXT, user_id INTEGER REFERENCES users);
+CREATE TABLE listings (id SERIAL PRIMARY KEY , header TEXT, location TEXT, content TEXT, user_id INTEGER REFERENCES users(id));
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
