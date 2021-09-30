64389a09ebd997a7b54fd1f18f1065dcb3d1c114
Henri Immonen <henri.immonen000@gmail.com>
2021-09-30 07:54:43 +0300

    csrf_token added for security

--- users.py
+++ users.py
@@ -1,9 +1,8 @@
 from db import db
-from flask import session, flash
+from flask import session, flash, secrets
 from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(username, password):
-    error = None
     sql = "SELECT id, password FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
@@ -18,11 +17,13 @@ def login(username, password):
     flash("You were successfully logged in!")
     session["user_id"] = user[0]
     session["user_name"] = username
+    session["csrf_token"] = secrets.token_hex(16)
     return True
 
 def logout():
     del session["user_id"]
     del session["user_name"]
+    del session["csrf_token"]
 
 def register(username, password):
     hash_value = generate_password_hash(password)
